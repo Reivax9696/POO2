d60e31053d142c3d06763cfeb707c97e135053c6
ModeRed55 <42684549+ModeRed55@users.noreply.github.com>
2024-01-22 23:00:40 +0100

    commit2

---
+++ POO2Nivell23.php
@@ -0,0 +1,78 @@
+<?php
+
+
+abstract class Shape {
+    public $alcada;
+    public $amplada;
+    public $radi;
+
+
+    public function __construct($alcada, $amplada){
+        $this->alcada = $alcada;
+        $this->amplada = $amplada;
+    }
+
+    public abstract function calcularArea();
+}
+
+
+Class Rectangle extends Shape {
+
+    function calcularArea(){
+        return $this->alcada*$this->amplada;
+    }
+
+}
+
+
+Class Triangle extends Shape {
+
+    public function calcularArea(){
+        return $this->alcada * $this->amplada / 2;
+    
+}
+}
+
+class Cercle extends Shape {
+    public $radi;
+
+    public function __construct($radi){
+        $this->radi = $radi;
+    }
+
+    public function calcularArea(){
+        return M_PI * $this->radi * $this->radi;
+    }
+}
+
+
+$triangle1 = new Triangle (10, 15);
+$rectangle1 = new Rectangle (10, 15);
+$cercle1 = new cercle(5);
+
+
+echo "L'area del triangle es " . $triangle1->calcularArea() . "<br>";
+echo "L'area del rectangle es " . $rectangle1->calcularArea() . "<br>";
+echo "L'area del cercle es " . $cercle1->calcularArea();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
